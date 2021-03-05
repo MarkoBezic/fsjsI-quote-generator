1bb4f862e85143a13e7eee63ae6d89b1138460f0
markobezic <marko.bezic@gmail.com>
2021-03-04 19:18:07 -0500

    removed extra quotation marks from quote array

--- js/script.js
+++ js/script.js
@@ -9,55 +9,55 @@ project 1 - A Random Quote Generator
 let quotes = [
   {
     quote:
-      "'If you don't produce, you won't thrive-no matter how skilled or talented you are'",
+      "If you don't produce, you won't thrive-no matter how skilled or talented you are",
     source: "Cal Newport",
   },
   {
-    quote: "'You can't steer a parked car'",
+    quote: "You can't steer a parked car",
     source: "unknown",
   },
   {
     quote:
-      "'You wasted $150,000 on an education you coulda got for $1.50 in late fees at the public library.'",
+      "You wasted $150,000 on an education you coulda got for $1.50 in late fees at the public library.",
     source: "Will Hunting",
     citation: "Van Sant, Gus. Good Will Hunting. Miramax, 1997.",
   },
   {
-    quote: "'Do what you can, with what you have, where you are.'",
+    quote: "Do what you can, with what you have, where you are.",
     source: "Theodore Roosevelt",
     year: "1913",
   },
   {
     quote:
-      "'Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough. ",
+      "Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough.",
     source: "Richard P. Feynman",
   },
   {
     quote:
-      "'Failure is unimportant. It takes courage to make a fool of yourself.'",
+      "Failure is unimportant. It takes courage to make a fool of yourself.",
     source: "Charlie Chaplin",
     tags:
       "#HUMOR #CHARLIECHAPLIN #THEATER #COMEDY #STAGE #FILM #MOVIES #HOLLYWOOD",
   },
   {
     quote:
-      "'Someone's sitting in the shade today because someone planted a tree a long time ago'",
+      "Someone's sitting in the shade today because someone planted a tree a long time ago",
     source: "Warren Buffet",
   },
   {
-    quote: "'Once I made a decision, I never thought about it again.'",
+    quote: "Once I made a decision, I never thought about it again.",
     source: "Michael Jordan",
   },
   {
-    quote: "'We first make our habits, and then our habits make us.'",
+    quote: "We first make our habits, and then our habits make us.",
     source: "John Dryden",
   },
   {
-    quote: "'If you fight for your limitations, you get to keep them'",
+    quote: "If you fight for your limitations, you get to keep them.",
     source: "Jim Kwik",
   },
   {
-    quote: "'It does not matter how slowly you go as long as you do not stop'",
+    quote: "It does not matter how slowly you go as long as you do not stop",
     source: "Confucius",
   },
 ];
